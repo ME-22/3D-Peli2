d0718e90fd0944a569840292cee187c25dba775e
kaneli <erjasalminen@live.com>
2021-02-11 18:17:17 +0200

    lumiukko

---
+++ New Unity Project/Assets/Scripts/MouseLook.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseLook : MonoBehaviour
+{
+    public float mouseSensitivity = 100f;
+    public Transform playerBody;
+    private float xRotation = 0f;
+    // Start is called before the first frame update
+    void Start()
+
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
