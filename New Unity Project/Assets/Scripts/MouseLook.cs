c2df995ed98e0f6c683a9d9dbdab112d505cc3fa
kaneli <erjasalminen@live.com>
2021-02-18 18:13:27 +0200

    hiiri asetuksia

--- New Unity Project/Assets/Scripts/MouseLook.cs
+++ New Unity Project/Assets/Scripts/MouseLook.cs
@@ -5,7 +5,10 @@ using UnityEngine;
 public class MouseLook : MonoBehaviour
 {
     public float mouseSensitivity = 100f;
+    public float minXAgle = -70f, maxXAngle = 80f;
     public Transform playerBody;
+
+
     private float xRotation = 0f;
     // Start is called before the first frame update
     void Start()
@@ -17,6 +20,13 @@ public class MouseLook : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation,minXAgle,maxXAngle);
+        transform.localRotation = Quaternion.Euler(xRotation,0,0);
+        playerBody.Rotate(Vector3.up * mouseX);
+
     }
 }
