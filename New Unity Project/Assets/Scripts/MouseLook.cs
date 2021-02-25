6c832676329912cdfff48592e8907edd623a940e
kaneli <erjasalminen@live.com>
2021-02-25 18:13:53 +0200

    hyppy ja kävely

--- New Unity Project/Assets/Scripts/MouseLook.cs
+++ New Unity Project/Assets/Scripts/MouseLook.cs
@@ -20,13 +20,18 @@ public class MouseLook : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        MouseLookAround();
+
+    }
+
+    private void MouseLookAround()
+    {
         float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
         float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
 
         xRotation -= mouseY;
-        xRotation = Mathf.Clamp(xRotation,minXAgle,maxXAngle);
-        transform.localRotation = Quaternion.Euler(xRotation,0,0);
+        xRotation = Mathf.Clamp(xRotation, minXAgle, maxXAngle);
+        transform.localRotation = Quaternion.Euler(xRotation, 0, 0);
         playerBody.Rotate(Vector3.up * mouseX);
-
     }
 }
---
+++ New Unity Project/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public CharacterController controller;
+
+    public float speed = 20f;
+
+    public float gravity = -9.82f;
+
+    public float jumpHeigt = 3f;
+
+    private Vector3 velocity = Vector3.zero;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * x + transform.forward * z;
+
+        controller.Move(move * speed * Time.deltaTime);
+
+        if (Input.GetButtonDown("jump"))
+        {
+            velocity.y = Mathf.Sqrt(jumpHeigt * -2f * gravity);
+        }
+
+        velocity.y += gravity + Time.deltaTime;
+
+        controller.Move(velocity * Time.deltaTime);
+    }
+}
