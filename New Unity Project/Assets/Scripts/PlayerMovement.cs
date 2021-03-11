89195b77fcbc40e44c9c4f6c248ad9c40255efdc
kaneli <erjasalminen@live.com>
2021-03-11 18:15:32 +0200

    asioita

--- New Unity Project/Assets/Scripts/PlayerMovement.cs
+++ New Unity Project/Assets/Scripts/PlayerMovement.cs
@@ -6,13 +6,23 @@ public class PlayerMovement : MonoBehaviour
 {
     public CharacterController controller;
 
+    public Transform groundCheck;
+
+    public float groundDistance = 0.4f;
+
+    public LayerMask GroundMask;
+
     public float speed = 20f;
 
     public float gravity = -9.82f;
 
-    public float jumpHeigt = 3f;
+    public float JumpHeigt = 3f;
 
     private Vector3 velocity = Vector3.zero;
+
+    private bool isGrounded;
+
+    private bool canMove;
 
     // Start is called before the first frame update
     void Start()
@@ -23,20 +33,44 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (!canMove){
+            return;
+        }
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, GroundMask);
+
+
         float x = Input.GetAxis("Horizontal");
         float z = Input.GetAxis("Vertical");
 
-        Vector3 move = transform.right * x + transform.forward * z;
+        Move(x,z);
 
-        controller.Move(move * speed * Time.deltaTime);
-
-        if (Input.GetButtonDown("jump"))
-        {
-            velocity.y = Mathf.Sqrt(jumpHeigt * -2f * gravity);
-        }
+        Jump();
 
         velocity.y += gravity + Time.deltaTime;
 
         controller.Move(velocity * Time.deltaTime);
     }
+
+    public void Move(float x, float z){
+
+        Vector3 move = transform.right * x + transform.forward * z;
+
+        controller.Move(move * speed * Time.deltaTime);
+    }
+
+    public void Jump() {
+
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            velocity.y = Mathf.Sqrt(JumpHeigt * -2f * gravity);
+        }
+
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(groundCheck.position, groundDistance);
+    }
+
 }
