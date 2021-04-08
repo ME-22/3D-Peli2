6b4a02482023d1f669eb1a309614272ce8545ad1
kaneli <erjasalminen@live.com>
2021-04-08 18:11:04 +0300

    aseenvaihto

--- New Unity Project/Assets/Scripts/Weapon.cs
+++ New Unity Project/Assets/Scripts/Weapon.cs
@@ -6,26 +6,64 @@ public class Weapon : MonoBehaviour
 {
     public Transform throwNode;
 
+    public float throwForce = 10f;
+
+    public GameObject throwObjPrefab;
+
     public FireArm fireArm;
 
+    private Camera FPSCamera;
+
+    public Transform weaponNode;
+
+    public List<Transform> firearms = new List<Transform>();
+
+    public int currentWeaponIndex = 0;
+
+
     // Start is called before the first frame update
+    
     void Start()
     {
+        FPSCamera = Camera.main;
 
+        foreach(Transform child in weaponNode)
+        {
+            if (child.GetComponent<FireArm>())
+            {
+                firearms.Add(child);
+            }
+        }
+
+        UpdateCurrentWeapon();
     }
 
     // Update is called once per frame
     void Update()
     {
-
-
-
         if (Input.GetButtonDown("Fire1"))
+        {
+            Throw();
+        }
+
+
+        if (Input.GetButtonDown("Fire2"))
         {
             Shoot();
         }
+
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            ChangeNextWeapon(1);
+        }
+
     }
 
+    private void Throw()
+    {
+        GameObject obj = Instantiate(throwObjPrefab, throwNode.position, Quaternion.identity);
+        obj.GetComponent<Rigidbody>().AddForce(FPSCamera.transform.forward * throwForce);
+    }
 
     public void Shoot()
     {
@@ -42,5 +80,33 @@ public class Weapon : MonoBehaviour
     public void SetFireArm(FireArm NewFireArm)
     {
         fireArm = NewFireArm;
+    }
+
+    public void ChangeNextWeapon(int nextIndex) 
+    {
+        currentWeaponIndex += nextIndex;
+
+        if(currentWeaponIndex > firearms.Count - 1) 
+        {
+            currentWeaponIndex = 0;
+        }
+
+        UpdateCurrentWeapon();
+    }
+
+    public void UpdateCurrentWeapon()
+    {
+        for(int i = 0; i < firearms.Count; i++)
+        {
+            if (currentWeaponIndex != i)
+            {
+                firearms[i].gameObject.SetActive(false);
+            }
+            else 
+            {
+                firearms[i].gameObject.SetActive(true);
+                fireArm = firearms[i].GetComponent<FireArm>();
+            }
+        }
     }
 }
